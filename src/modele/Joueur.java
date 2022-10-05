2c016d2ee0d381a4e7a29067ba3b48ceae189621
DionSofiane <sofiane.dion@laposte.net>
2022-10-05 14:57:49 +0200

    :sparkles:(player) add methods

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 
 public class Joueur {
 	private String nom;
@@ -18,51 +19,37 @@ public class Joueur {
 		this.tresor = 0;
 		this.nbQuartiers = 0;
 		this.possedeCouronne = false;
-		this.cite = new Quartier [8];
+		this.cite = new Quartier[8];
 		this.main = new ArrayList<Quartier>();
-	}
-
-
-	public int ajouterPieces(int nbPieces) {
-		if (nbPieces >= 0) {
-			tresor += nbPieces;
-		}
-		return tresor;
-	}
-
-	public int retirerPieces(int nbPieces) {
-		if (nbPieces >= 0) {
-			if (tresor - nbPieces < 0) {
-				tresor = 0;
-			} else {
-				tresor -= nbPieces;
-			}
-		}
-		return tresor;
 	}
 
 	public String getNom() {
 		return this.nom;
 	}
-	
+
 	public int nbPieces() {
 		return this.tresor;
 	}
 
-	public long nbQuartiersDansCite() {
-		return Arrays.stream(this.cite).filter(Objects::nonNull).count();
+	public int nbQuartiersDansCite() {
+		int count = 0;
+		for (int i = 0; i < this.cite.length; i++) {
+			if (this.cite[i] != null) {
+				count++;
+			}
+		}
+		return count;
 	}
-	
+
 	public Quartier[] getCite() {
 		return this.cite;
 	}
-	
+
 	public List<Quartier> getMain() {
 		return this.main;
 	}
-	
 
-	public long nbQuartiersDansMain() {
+	public int nbQuartiersDansMain() {
 		return this.main.size();
 	}
 
@@ -73,4 +60,86 @@ public class Joueur {
 	public void setPossedeCouronne(boolean possedeCouronne) {
 		this.possedeCouronne = possedeCouronne;
 	}
+
+	public int ajouterPieces(int nbPieces) {
+		if (nbPieces >= 0) {
+			this.tresor += nbPieces;
+		}
+		return this.tresor;
+	}
+
+	public int retirerPieces(int nbPieces) {
+		if (nbPieces >= 0) {
+			if (nbPieces <= this.tresor) {
+				this.tresor -= nbPieces;
+			} else if (this.tresor - nbPieces < 0) {
+				return this.tresor;
+			}
+		}
+		return this.tresor;
+	}
+
+	public Quartier[] ajouterQuartierDansCite(Quartier quartier) {
+		for (int i = 0; i < this.cite.length; i++) {
+			if (this.cite[i] == null) {
+				this.cite[i] = quartier;
+				break;
+			}
+		}
+		return this.cite;
+	}
+
+	public boolean quartierPresentDansCite(String nom) {
+		for (int i = 0; i < this.cite.length; i++) {
+			if (this.cite[i] != null) {
+				if (this.cite[i].getNom() == nom) {
+					return true;
+				}
+			}
+
+		}
+		return false;
+	}
+
+	public Quartier retirerQuartierDansCite(String nom) {
+		Quartier removeQuartier;
+		for (int i = 0; i < this.cite.length; i++) {
+			if (this.cite[i] != null) {
+				if (this.cite[i].getNom() == nom) {
+					removeQuartier = this.cite[i];
+					this.cite[i] = null;
+					return removeQuartier;
+				}
+			}
+		}
+		return null;
+
+	}
+
+	public void ajouterQuartierDansMain(Quartier quartier) {
+		this.main.add(quartier);
+	}
+
+	public Quartier retirerQuartierDansMain() {
+		Quartier removeQuartier;
+		Random generateur = new Random();
+		int numeroHasard = generateur.nextInt(this.nbQuartiersDansMain());
+		if (this.main == null) {
+			return null;
+		} else {
+			removeQuartier = this.main.get(numeroHasard);
+			this.main.remove(numeroHasard);
+			return removeQuartier;
+
+		}
+	}
+
+	public void reinitialiser() {
+		this.tresor = 0;
+		this.nbQuartiers = 0;
+		this.possedeCouronne = false;
+		this.cite = new Quartier[8];
+		this.main = new ArrayList<Quartier>();
+	}
+
 }
