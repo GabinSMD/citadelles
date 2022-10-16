1281bec4dbb3317d38ee4f1ae575bf80db2c8051
DionSofiane <sofiane.dion@laposte.net>
2022-10-16 02:29:55 +0200

    :white_check_mark:(condottiere) update code and pass tests

--- src/modele/Condottiere.java
+++ src/modele/Condottiere.java
@@ -9,37 +9,67 @@ public class Condottiere extends Personnage {
 	}
 
 	public void utiliserPouvoir() {
-		int max = 0;
-		if (this.joueur != null) {
+		int max = 1;
+		if (this.getJoueur().getNom() != null) {
 			System.out.println("Voulez-vous utiliser votre pouvoir de destruction ?");
-			boolean choixPouvoir=Interaction.lireOuiOuNon();
+			boolean choixPouvoir = Interaction.lireOuiOuNon();
 			if (choixPouvoir == true) {
 				System.out.println("Voici la liste des joueurs et le contenu de leur cité :");
 				for (int i = 0; i < this.getPlateau().getNombreJoueurs(); i++) {
+					max++;
 					if (getPlateau().getJoueur(i) != null) {
-						System.out.println("Voici la liste des joueurs et le contenu de leur cité :");
-						System.out.println(i + " " + getPlateau().getJoueur(i).getNom() + ": ");
-						for (int j = 0; j < this.getPlateau().getJoueur(j).getCite().length; j++) {
-							System.out.println(i + " " + getPlateau().getJoueur(i).getCite()[j].getNom() + "(coût " + getPlateau().getJoueur(i).getCite()[j].getNom()+"), ");
+						System.out.print(i + 1 + " " + getPlateau().getJoueur(i).getNom() + ": ");
+						for (int j = 0; j < this.getPlateau().getJoueur(i).nbQuartiersDansCite(); j++) {
+							System.out.print(j + 1 + " " + getPlateau().getJoueur(i).getCite()[j].getNom() + "(coût "
+									+ getPlateau().getJoueur(i).getCite()[j].getCout() + "), ");
 						}
-						System.out.println("Pour information vous avez " + this.getPlateau().getJoueur(i).nbPieces()
-								+ " pièces d'or dans votre trésor");
-						max++;
+						System.out.println("");
 					}
 				}
+				System.out.println("Pour information vous avez " + this.getJoueur().nbPieces() + " pièces d'or dans votre trésor");
 				System.out.println("Quel joueur choisissez-vous ? (0 pour ne rien faire)");
-				int choixJoueur = Interaction.lireUnEntier(0, max);
-				System.out.println("Quel quartier choisissez-vous ?");
+				boolean checkEveque = false;
+				int choixJoueur;
+				do {
+					choixJoueur = Interaction.lireUnEntier(0, max+1);
+					if (choixJoueur == 0) {
+						break;
+					}
+					choixJoueur -= 1;
+					if (this.getPlateau().getJoueur(choixJoueur).getPersonnage().getCaracteristiques() == Caracteristiques.EVEQUE) {
+
+						if (this.getPlateau().getJoueur(choixJoueur).monPersonnage.getAssassine()==true) {
+							System.out.println("L'évêque a été assassiné, il est grand temps de pillé ses biens !");
+							checkEveque = true;
+						} else {
+							checkEveque = false;
+							System.out.println("Vous ne pouvez cibler l'évêque.");
+							System.out.println("Votre choix ? (0 pour annuler)");
+						}
+					} else {
+						checkEveque = true;
+					}
+				} while(checkEveque == false);
+				System.out.println("Quel quartier choisissez-vous ? (0 pour annuler)");
 				boolean check = false;
 				do {
-					int choixQuartier = Interaction.lireUnEntier(0, max);
+					int choixQuartier = Interaction.lireUnEntier(0, getPlateau().getJoueur(choixJoueur).nbQuartiersDansCite()+1);
+					if (choixQuartier == 0) {
+						break;
+					} 
+					choixQuartier -= 1;
+					String nomQuartier = this.getPlateau().getJoueur(choixJoueur).getCite()[choixQuartier].getNom();
 					int verifCoutQuartier = this.getPlateau().getJoueur(choixJoueur).getCite()[choixQuartier].getCout();
 					if (verifCoutQuartier - 1 > this.getJoueur().nbPieces()) {
 						System.out.println("Votre trésor n'est pas suffisant");
-						System.out.println("Votre choix ?");
+						System.out.println("Votre choix ? (0 pour annuler)");
 						check = false;
 					} else {
 						check = true;
+						this.getJoueur().retirerPieces(verifCoutQuartier - 1);
+						this.getPlateau().getJoueur(choixJoueur).retirerQuartierDansCite(nomQuartier);
+						System.out.println("=> On retire  " + nomQuartier + " à " + this.getPlateau().getJoueur(choixJoueur).getNom());
+						System.out.println("Pour information vous avez " + this.getJoueur().nbPieces() + " pièces d'or dans votre trésor");
 					}
 				} while (check == false);
 			}
@@ -50,13 +80,13 @@ public class Condottiere extends Personnage {
 		int nbQuartierMilitaire = 0;
 
 		super.percevoirRessourcesSpecifiques();
-		if (this.joueur != null) {
+		if (this.getJoueur().getNom() != null) {
 
-			Quartier[] Cite = new Quartier[this.joueur.nbQuartiersDansCite()];
+			Quartier[] Cite = new Quartier[this.getJoueur().nbQuartiersDansCite()];
 
 			for (int i = 0; i < Cite.length; i++) {
-				for (int j = 0; j < this.joueur.getCite().length; j++) {
-					Cite[i] = this.joueur.getCite()[i];
+				for (int j = 0; j < this.getJoueur().getCite().length; j++) {
+					Cite[i] = this.getJoueur().getCite()[i];
 				}
 			}
 			for (int k = 0; k < Cite.length; k++) {
@@ -64,7 +94,7 @@ public class Condottiere extends Personnage {
 					nbQuartierMilitaire++;
 				}
 			}
-			this.joueur.ajouterPieces(nbQuartierMilitaire);
+			this.getJoueur().ajouterPieces(nbQuartierMilitaire);
 			System.out.println("[" + nbQuartierMilitaire + "]" + "pieces en plus dans votre trésor");
 		}
 	}
