49d5f627b3f2c0f8ae6c8b10872f28e1d954ee12
kilian <kilian1.laborderie@gmail.com>
2022-10-26 15:45:16 +0200

    ✨(assassin) add new methode utiliserPouvoirAvatar()

--- src/modele/Assassin.java
+++ src/modele/Assassin.java
@@ -1,4 +1,6 @@
 package modele;
+import java.util.Random;
+
 import controleur.Interaction;
 
 public class Assassin extends Personnage {
@@ -18,8 +20,33 @@ public class Assassin extends Personnage {
 			}
 		}
 		System.out.println("Votre choix :");
-		int choix =Interaction.lireUnEntier(0,max);
+		int choix =Interaction.lireUnEntier(0,max+1);
 		this.getPlateau().getPersonnage(choix).setAssassine();
+		return;
+	}
+	
+	public void utiliserPouvoirAvatar() {
+		int max = 0;
+		Random r = new Random();
+		Personnage[] selection = new Personnage[9];
+		for(int i=0; i<9; i++) {
+			if(this.getPlateau().getPersonnage(i)!= null && this.getPlateau().getPersonnage(i).getNom() != this.getNom()) {
+				
+				selection[max]=this.getPlateau().getPersonnage(i);
+				
+				max++;
+			}
+		}
+		/* test contenu tableau de selection
+		 * for(int i=0; i<max; i++) {
+			if(selection[0]!=null) {
+				System.out.println("selection index: "+i+" "+selection[i]);
+			}
+		}*/
+
+		int choix =r.nextInt(max);
+		selection[choix].setAssassine();
+		System.out.println("Le/La "+selection[choix].getNom()+" a été assassiné.e");
 		return;
 	}
 }
