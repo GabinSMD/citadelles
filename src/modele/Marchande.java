d56ce25439b7a907ef8654f2cbd6a990b375f67c
kilian <kilian1.laborderie@gmail.com>
2022-11-28 20:42:49 +0100

    ♻️💡(merchant) refactor, modify code comments

--- src/modele/Marchande.java
+++ src/modele/Marchande.java
@@ -6,33 +6,34 @@ public class Marchande extends Personnage {
 		super("Marchande", 6, Caracteristiques.MARCHANDE);
 	}
 	
+	// Utilisation du pouvoir par un joueur humain
 	public void utiliserPouvoir() {
 		if(this.getJoueur()!=null) {
 			this.getJoueur().ajouterPieces(1);
 		}
 	}
-	//Pas de possibilité de mettre de l'aléatoire
+	
+	// Utilisation du pouvoir par un avatar
 	public void utiliserPouvoirAvatar() {
 		if(this.getJoueur()!=null) {
 			this.getJoueur().ajouterPieces(1);
 		}
 	}
-
+	
+	// Perception des ressources spécifiques
 	public void percevoirRessourcesSpecifiques() {
+		super.percevoirRessourcesSpecifiques();
 		int nbQuartierCommercant=0;
 		
-		super.percevoirRessourcesSpecifiques();
 		if(this.getJoueur()!=null) {
-			
 			Quartier[] Cite = new Quartier[this.getJoueur().nbQuartiersDansCite()];
-			
 			for(int i =0;i<Cite.length;i++) {
 				for(int j=0;j<this.getJoueur().getCite().length;j++) {
 					Cite[i]=this.getJoueur().getCite()[i];
 				}
 			}
-			for(int k =0;k<Cite.length;k++) {
-				if(Cite[k].getType()=="COMMERCANT") {
+			for(int i =0;i<Cite.length;i++) {
+				if(Cite[i].getType()=="COMMERCANT") {
 					nbQuartierCommercant++;
 				}
 			}
