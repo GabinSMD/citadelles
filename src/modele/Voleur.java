7dbe09adae488d4bf0ca2f6e9277cf45737b2ba9
kilian <kilian1.laborderie@gmail.com>
2022-12-02 22:41:32 +0100

    ♻️(thief) refactor and fix bug

--- src/modele/Voleur.java
+++ src/modele/Voleur.java
@@ -8,6 +8,7 @@ public class Voleur extends Personnage {
 	private int choix=0;
 	private int max = 0;
 	private Personnage[] selection = new Personnage[9];
+	boolean choixPouvoir= true;
 
 	public Voleur() {
 		super("Voleur", 2, Caracteristiques.VOLEUR);
@@ -15,58 +16,80 @@ public class Voleur extends Personnage {
 	}
 
 	public void utiliserPouvoir() {
-		System.out.println("Quel personnage voulez-vous voler ?");
-		//Affichage de la liste des personnages selectionnables
-		for(int i=0; i<9; i++) {
-			if(this.getPlateau().getPersonnage(i)!= null) {
-				selection[i]=this.getPlateau().getPersonnage(i);
-				System.out.println(i+1+"."+ selection[i].getNom());
-				max++;
-			}
-		}
-		System.out.println("Votre choix :");
-		//Choix du personnage que l'on souhaite voler
-		//NB: LE voleur ne pourra pas être volé ainsi que les personnages de rang 1, le joueur en sera averti et devra choisir une nouvelle cible
-		do {
-			choix =Interaction.lireUnEntier(1,max+1);
-			if(selection[choix-1].getNom()=="Voleur") {
-				System.out.println("Le voleur ne peut pas être selectionné");
-			}
-			else if(selection[choix-1].getRang()==1) {
-				System.out.println("Le personnage est de rang 1, il ne peut donc pas être selectionné");
-			}
-		}while(selection[choix-1].getNom()=="Voleur" || selection[choix-1].getRang()==1);	
+		this.max=0;
+		this.choix=0;
 		
-		//Vol de la cible
-		selection[choix-1].setVole();
-		System.out.println("Le/La "+selection[choix-1].getNom()+" a été volé.e");
-		return;
+		if (this.getJoueur() != null) {
+			System.out.println("Voulez-vous utiliser votre pouvoir ?");
+			this.choixPouvoir = Interaction.lireOuiOuNon();
+			if (this.choixPouvoir == true) {
+				System.out.println("Quel personnage voulez-vous voler ?");
+				//Affichage de la liste des personnages selectionnables
+				for(int i=0; i<9; i++) {
+					if(this.getPlateau().getPersonnage(i)!= null) {
+						this.selection[i]=this.getPlateau().getPersonnage(i);
+						System.out.println(i+1+"."+ this.selection[i].getNom());
+						this.max++;
+					}
+				}
+				System.out.println("Votre choix :");
+				//Choix du personnage que l'on souhaite voler
+				//NB: LE voleur ne pourra pas être volé ainsi que les personnages de rang 1, le joueur en sera averti et devra choisir une nouvelle cible
+				do {
+					this.choix =Interaction.lireUnEntier(1,this.max+1);
+					if(this.selection[this.choix-1].getNom()=="Voleur") {
+						System.out.println("Le voleur ne peut pas être selectionné");
+					}
+					else if(this.selection[this.choix-1].getRang()==1) {
+						System.out.println("Le personnage est de rang 1, il ne peut donc pas être selectionné");
+					}
+				}while(this.selection[choix-1].getNom()=="Voleur" || this.selection[choix-1].getRang()==1);	
+				
+				//Vol de la cible
+				this.selection[this.choix-1].setVole();
+				System.out.println("Le/La "+this.selection[this.choix-1].getNom()+" a été volé.e");
+			}
+			else {
+				System.out.println("Vous n'utilisez pas votre pouvoir");
+			}
+		}else {
+			System.out.println("Un Joueur n'a pas été attribué au Voleur");
+		}
 	}
 	
 	public void utiliserPouvoirAvatar() {
 		Random r = new Random();
+		this.max=0;
+		this.choix=0;
 		//Affichage de la liste des personnages selectionnables
 		for(int i=0; i<9; i++) {
 			if(this.getPlateau().getPersonnage(i)!= null) {
-				selection[i]=this.getPlateau().getPersonnage(i);
-				max++;
+				this.selection[i]=this.getPlateau().getPersonnage(i);
+				this.max++;
 			}
 		}
-		//Choix du personnage que l'on souhaite voler
-		//NB: LE voleur ne pourra pas être volé ainsi que les personnages de rang 1, le joueur en sera averti et devra choisir une nouvelle cible
-		do {
-			choix =r.nextInt(max);
-			if(selection[choix].getNom()=="Voleur") {
-				System.out.println("Le tente de se voler lui même ( pas fût fût :/ ) \nIl cherche une autre cible");
-			}
-			else if(selection[choix].getRang()==1) {
-				System.out.println("Le voleur tente de voler un rang 1 \nIl échoue et cherche une nouvelle cible");
-			}
-		}while(selection[choix].getNom()=="Voleur" || selection[choix].getRang()==1);	
-		
-		//Vol de la cible
-		selection[choix].setVole();
-		System.out.println("Le/La "+selection[choix].getNom()+" a été volé.e");
-		return;
+		this.choix =r.nextInt(this.max);
+		switch(this.choix) {
+		case 0:
+			System.out.println("Le Voleur n'utilise pas son pouvoir");
+			break;
+		case 1:
+			//Choix du personnage que l'on souhaite voler
+			//NB: LE voleur ne pourra pas être volé ainsi que les personnages de rang 1, le joueur en sera averti et devra choisir une nouvelle cible
+			do {
+				this.choix =r.nextInt(this.max);
+				if(this.selection[this.choix].getNom()=="Voleur") {
+					System.out.println("Le tente de se voler lui même ( pas fût fût :/ ) \nIl cherche une autre cible");
+				}
+				else if(this.selection[this.choix].getRang()==1) {
+					System.out.println("Le voleur tente de voler un rang 1 \nIl échoue et cherche une nouvelle cible");
+				}
+			}while(this.selection[this.choix].getNom()=="Voleur" || this.selection[this.choix].getRang()==1);	
+			
+			//Vol de la cible
+			this.selection[this.choix].setVole();
+			System.out.println("Le/La "+this.selection[this.choix].getNom()+" a été volé.e");
+			break;
+		}
 	}
 }
